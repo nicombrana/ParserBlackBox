59105a64e46033b5c77fc63cadfba0e965d7e006
Nico B <nicolasmbrana@gmail.com>
2018-08-29 19:58:11 -0300

    Formalizing the last test and removing it from the code

--- parser/PossibleParser.py
+++ parser/PossibleParser.py
@@ -64,24 +64,3 @@ def parseLogText(aLogText):
         for similar in similarLines:
             finalVector.append(findParamValueAndReplaceIfSimilarLines(line, similar, 2))
     return set(finalVector)
-
-
-#------------------Obvio que no hice tests al principio y ahora estoy dejando esto porque puedo------------------#
-actualLogExtraction = """(h-checkout-v1-04) 2018-02-21 05:00:04,012 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)domain.util.timetracking.MeasureTimeAspect (ChasFacadeServiceImpl.java:28) : The Method chas.getRoomPacksV3 took 278 to run
-(h-checkout-v1-04) 2018-02-21 05:00:04,022 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:39) : AbtestpriceWithoutSurprises does not apply so returns default branch
-(h-checkout-v1-04) 2018-02-21 05:00:04,023 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=50, abName=hurryUp, forceUpdate=false}
-(h-checkout-v1-04) 2018-02-21 05:00:04,023 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:52) : abTestResult was found in cookies with value: AbTestResult{abTestName=hurryUp, abBranchWinner=branchB, apply=true, branchAWon=false, configurationsMap={banner=ab-HurryUp.ftl}}
-(h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=100, abName=chanchito, forceUpdate=false}
-(h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:52) : abTestResult was found in cookies with value: AbTestResult{abTestName=chanchito, abBranchWinner=branchA, apply=true, branchAWon=true, configurationsMap={banner=ab-dummy.ftl}}
-(h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDt was succesfully retrieved with value: AbTestDto{upperBound=50, abName=staypopup, forceUpdate=false}"""
-result = parseLogText(actualLogExtraction)
-print(result)
-
-
-
-print("--------------------------------")
-newLog = """5561 warn checkPermissions Missing write access to /usr/lib/node_modules/npm/node_modules/npmlog/node_modules
-5562 warn checkPermissions Missing write access to /usr/lib/node_modules/npm/node_modules/osenv/node_modules
-5563 warn checkPermissions Missing write access to /usr/lib/node_modules/npm/node_modules/read-package-json/node_modules/glob/node_modules/minimatch/node_modules/brace-expansion/node_modules
-5564 Why God, why?"""
-print(parseLogText(newLog))
--- parser/PossibleParserTests.py
+++ parser/PossibleParserTests.py
@@ -45,3 +45,18 @@ class TestPossibleParser(unittest.TestCase):
         informationLine = PossibleParser.removeTagsFromLineAfterToken(aCompleteLine, " : ")
 
         self.assertEqual(informationLine, "AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=50, abName=hurryUp, forceUpdate=false}")
+
+
+    def testGivenALogTextAnArrayOfStructuredLogLinesIsReturned(self):
+        aLogText = """(h-checkout-v1-04) 2018-02-21 05:00:04,012 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)domain.util.timetracking.MeasureTimeAspect (ChasFacadeServiceImpl.java:28) : The Method chas.getRoomPacksV3 took 278 to run
+        (h-checkout-v1-04) 2018-02-21 05:00:04,022 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:39) : AbtestpriceWithoutSurprises does not apply so returns default branch
+        (h-checkout-v1-04) 2018-02-21 05:00:04,023 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=50, abName=hurryUp, forceUpdate=false}
+        (h-checkout-v1-04) 2018-02-21 05:00:04,023 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:52) : abTestResult was found in cookies with value: AbTestResult{abTestName=hurryUp, abBranchWinner=branchB, apply=true, branchAWon=false, configurationsMap={banner=ab-HurryUp.ftl}}
+        (h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=100, abName=chanchito, forceUpdate=false}
+        (h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:52) : abTestResult was found in cookies with value: AbTestResult{abTestName=chanchito, abBranchWinner=branchA, apply=true, branchAWon=true, configurationsMap={banner=ab-dummy.ftl}}
+        (h-checkout-v1-04) 2018-02-21 05:00:04,024 INFO [1DUv9FFU-hTeHfH50-h-checkout-v1-04-63160-CRl4uePK0m] [c1a09542-3493-409d-9edd-12a8ca98dce6] [http-9290-39] (...)hotels.domain.abtest.AbTestService (AbTestService.java:43) : AbtestDto was succesfully retrieved with value: AbTestDto{upperBound=50, abName=staypopup, forceUpdate=false}"""
+
+        structuredLineArray = PossibleParser.parseLogText(aLogText)
+        expectedStructuredLineArray = ['The Method chas.getRoomPacksV3 took 278 to run', 'AbtestpriceWithoutSurprises does not apply so returns default branch', 'AbtestDto was succesfully retrieved with value: *', 'abTestResult was found in cookies with value: *']
+
+        self.assertEqual(structuredLineArray, expectedStructuredLineArray)
